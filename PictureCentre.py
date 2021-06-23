dbd2eee915c44432f6ee78b6049603aa4558f516
Thomas <thomas.blindert@stud.hs-bochum.de>
2021-06-23 14:48:40 +0200

    Update Version to 1.2

--- PictureCentre.py
+++ PictureCentre.py
@@ -271,7 +271,7 @@ def geodround(value, decimal):
 
 ########################################################################################################################
 
-__version__ = "1.1"
+__version__ = "1.2"
 __author__ = "Github: Thomas-Atachon"
 
 ''' Evaluate Parameters '''
